ba0e1899679e09e5aa7c39763a742e42a41d3bf9
rick22 Alcazar <rickity_rick_22@hotmail.com>
2017-09-14 01:48:09 -0700

    twitter not working

--- Liri.js
+++ Liri.js
@@ -4,4 +4,32 @@ var spotify = require('spotify');
 var request = require('request');
 
 // script
-var keys = require("/keys.js")
+var keys = require("./keys.js")
+
+var app = {
+        "my-tweets": function() {
+            var client = new Twitter(keys);
+            client.get('statuses/user_timeline', function(error, tweetData, response) {
+                if (!error) {
+                    console.log(' ');
+                    console.log('My Tweets');
+                    tweetData.forEach(function(obj) {
+                        console.log('Time: ' + obj.created_at);
+                        console.log('Tweet: ' + obj.text);
+                        console.log(' ');
+                    });
+                    console.log(' ');
+                    // console.log(tweets);
+
+                    app.logData(tweetData);
+                } else {
+                    console.log(error);
+                }
+            });
+        },
+        "spotify-this-song": function(keyword) {
+                spotify.search({ type: 'track', query: keyword || 'The Sign Ace of Base' }, function(err, data) {
+                            if (err) {
+                                console.log('Error occurred: ' + err);
+                                return;
+                            }
---
+++ keys.js
@@ -0,0 +1,10 @@
+console.log('this is loaded');
+
+var twitterKeys = {
+    consumer_key: '<v2moEhWIsDLpQrBRADCUJuIJC>',
+    consumer_secret: '<1NYAwlX0KC65TV3K5Q0Tb6CyL42l6QwAbyIPENRSARIh7OuJ4c>',
+    access_token_key: '<2956522111-bSKcnleE1r2KZvfPZcRgrbh7vUQJM9lciGhTd2s>',
+    access_token_secret: '<aNAZ6rMCE12Gqz1uzpZ1w0oCO5ZZQkJcWKOOFYIV2nYeh>',
+}
+
+module.exports = twitterKeys;
